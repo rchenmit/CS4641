a2326b57f392f4304fc41c94fd2a612bed73d306
Robert Chen <robchen401@gmail.com>
2014-04-17 15:30:30 -0400

    adding notes taken

---
+++ lec1_notes.R
@@ -0,0 +1,9 @@
+supervised y = f(x); given f, find x;
+	ex. 1^2 = 1, 2^2 = 4,x = 10, x^2 = 100
+unsupervised f(x); given x, find f
+reinforcement: come up with a set of actions you can actually take
+	a = f(x) ; T, R, A (information about the world)
+	you are NOT given a or f, you are given s, T, R, A and you have to find a, 
+	and f, which MAP s, T, R, A (the world) into a set of functions
+
+
